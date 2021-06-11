8f40a649211d9dc84a3a11ef8fb6a0d947d413e4
Ashish <ashm@stanford.edu>
2021-06-11 13:20:24 -0700

    working functions

--- R/codebook_preproc.R
+++ R/codebook_preproc.R
@@ -13,16 +13,23 @@
 codebook_renamer <- function(df, names_from, names_to){
   if(length(names_from) != length(names_to)){stop(paste("names_from and names_to lengths must match"))}
   codebook_data_match_idx <- names_from %in% names(df)
-  if(sum(codebook_data_match_idx) < length(names(df))){
-    msg <- paste("Warning:", sum(!codebook_data_match_idx), "items found in names_from that are not valid columns in df.")
+  if(sum(codebook_data_match_idx) < length(codebook_data_match_idx)){
+    msg <- paste("Warning:", sum(!codebook_data_match_idx), "item(s) found in names_from not found in df. \n",
+                 paste(names_from[!codebook_data_match_idx], collapse=" "))
     warning(msg)
-    warning(names_from[!codebook_data_match_idx])
   }
+  data_codebook_match_idx <- names(df) %in% names_from
+  if(sum(data_codebook_match_idx) < length(data_codebook_match_idx)){
+    msg <- paste("Warning:", sum(!data_codebook_match_idx), "columns(s) found in df not found in names_from. \n",
+                 paste(names(df)[!data_codebook_match_idx], collapse=" "))
+    warning(msg)
+  }
+
   complete_codebook_lines_idx <- !is.na(names_from) & !is.na(names_to)
-  names_from <- names_from[data_codebook_match_idx & complete_codebook_lines_idx]
-  names_to <- names_to[data_codebook_match_idx & complete_codebook_lines_idx]
+  names_from <- names_from[codebook_data_match_idx & complete_codebook_lines_idx]
+  names_to <- names_to[codebook_data_match_idx & complete_codebook_lines_idx]
   df %>%
-    dplyr::rename_at(vars(names_from), ~names_to)
+    dplyr::rename_at(dplyr::vars(dplyr::all_of(names_from)), ~names_to)
 }
 
 
@@ -31,33 +38,37 @@ codebook_renamer <- function(df, names_from, names_to){
 #' Recode the values of multiple columns .
 #'
 #' @param df Dataframe of data to be recoded.
-#' @param var_regexes Vector of regex patterns describing columns to recode values.
+#' @param var_regex Vector of regex patterns describing columns to recode values.
 #' @param values_from Vector of possible values to be recoded.
 #' @param values_to Vector of values to recode values_from into.
 #' @param mutate_fun Function to to apply to column after recoding. Set to NULL if no function is desired.
 #'
 #' @return A dataframe with recoded values.
+#' @importFrom rlang .data
+#'
 #' @export
 #'
 #' @examples
-codebook_recoder <- function(df, var_regexes, values_from, values_to, mutate_fun=as.numeric){
-  if(length(var_regexes) != length(values_from)){stop(paste("var_regexes and values_from lengths must match"))}
-  if(length(var_regexes) != length(values_to)){stop(paste("var_regexes and values_to lengths must match"))}
+codebook_recoder <- function(df, var_regex, values_from, values_to, mutate_fun=as.numeric){
+  if(length(var_regex) != length(values_from)){stop(paste("var_regex and values_from lengths must match"))}
+  if(length(var_regex) != length(values_to)){stop(paste("var_regex and values_to lengths must match"))}
   if(length(values_from) != length(values_to)){stop(paste("values_from and values_to lengths must match"))}
 
-  preproc <- function(x){x %>% str_replace_all(., "\\\r|\\\n", "") %>% tolower}
+  preproc <- function(x){stringr::str_replace_all(x, "\\\r|\\\n", "") %>% tolower}
 
-  var_regexes <- unique(var_regexes)
+  var_regex <- unique(var_regex)
   values_from <- preproc(values_from)
 
-  for(var in var_regexes){
-    level_key <- tibble::tibble(values_from = values_from[values_from == var],
-                                values_to = values_to[values_from == var]) %>%
-      tibble::deframe
+  for(current_regex in var_regex){
+    # current_vars <- str_extract(names(df), paste0(".*", current_regex,".*"))
+    level_key <- tibble::tibble(v_from = values_from[var_regex == current_regex],
+                                v_to = values_to[var_regex == current_regex]) %>%
+      tibble::deframe()
 
     df <- df %>%
-      dplyr::mutate_at(vars(matches(var)), ~preproc) %>%
-      if(!is.null(mutate_fun)){df <- df %>% dplyr::mutate_at(vars(matches(var)), mutate_fun)}
+      dplyr::mutate_at(dplyr::vars(tidyselect::matches(current_regex)), preproc) %>%
+      dplyr::mutate_at(dplyr::vars(tidyselect::matches(current_regex)), ~dplyr::recode(., !!!level_key))
+    if(!is.null(mutate_fun)){df <- df %>% dplyr::mutate_at(dplyr::vars(tidyselect::matches(current_regex)), mutate_fun)}
   }
   return(df)
 }
--- R/tally_scale.R
+++ R/tally_scale.R
@@ -10,41 +10,47 @@
 #' @param tally_function Function used to tally scores, e.g. mean, sum, etc.
 #'
 #' @return A dataframe with tallyed scales appended or dataframe with tallyed scales alone
+#' @importFrom rlang :=
+#' @importFrom rlang !!
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
 #' @export
 #'
 #' @examples
 #'
-#' df <- read.csv("datafile.csv")
+#' library(dplyr)
+#' df <- tribble(
+#' ~scale_sub1_1, ~scale_subscale1_2, ~scale_subscale2_1, ~scale_subscale2_2,
+#' 1,             2,                  3,                  3,
+#' 2,             3,                  2,                  3,
+#' 3,             2,                  3,                  3
+#' )
 #' df %>%
-#'    tally_scale("^tas_(dif|ddf|eot)_[0-9]{1,2}$", "tas") %>%
-#'    tally_scale("^tas_dif_[0-9]{1,2}$", "tas_dif") %>%
-#'    tally_scale("^tas_ddf_[0-9]{1,2}$", "tas_ddf") %>%
-#'    tally_scale("^tas_eot_[0-9]{1,2}$", "tas_eot")
+#'    tally_scale("^scale_(subscale1|subscale2)_[0-9]$", "scale_total") %>%
+#'    tally_scale("^scale_subscale1_[0-9]$", "scale_subscale1_total") %>%
+#'    tally_scale("^scale_subscale2_[0-9]$", "scale_subscale2_total")
 #'
-tally_scale <- function(df,
-                        var_regex,
-                        new_var_name=NULL,
-                        join_function=dplyr::full_join,
-                        na.rm=F,
-                        tally_function=sum){
+tally_scale <- function(df,var_regex, new_var_name=NULL, join_function=dplyr::full_join, na.rm=F, tally_function=sum){
   if(is.null(new_var_name)) new_var_name <- var_regex
-  cols_to_tally <- names(df)[str_detect(names(df), var_regex)] # identify columns to aggregate
-  print(paste0("[", new_var_name, "] tallying ", length(cols_to_tally), " columns:"))
-  print(cols_to_tally)
-  cat("\n")
+  cols_to_tally <- names(df)[stringr::str_detect(names(df), var_regex)] # identify columns to aggregate
+  cat(paste0("[", new_var_name, "] ", length(cols_to_tally), " columns:\n"))
+  cat(paste0("\t"))
+  cat(paste0(cols_to_tally))
+  cat("\n\n")
   df <- df %>%
-    dplyr::select(one_of(cols_to_tally))  # select columns to aggregate
-  if(any(purrr::map_dfc(df, ~grepl("[^0-9.]", .)))){stop("Non-numeric values found in df")}
-  df <- df %>%
+    dplyr::mutate(!!"tmp_row_id" := 1:dplyr::n()) # add a temporary id column
+  df_tally <- df %>%
+    dplyr::select(.data$tmp_row_id, dplyr::one_of(cols_to_tally))  # select columns to aggregate
+  if(any(purrr::map_dfc(df, ~grepl("[^0-9.]", .)))){stop("Non-numeric values found in columns")}
+  df_tally <- df_tally %>%
     dplyr::mutate_all(as.numeric) %>% # convert to numeric data type
-    dplyr::mutate(..temp_id_col.. = 1:n()) %>% # add a temporary id column
-    tidyr::pivot_longer(-..temp_id_col..) %>% # convert to long format
-    dplyr::group_by(..temp_id_col..) %>% # group by participant
-    dplyr::summarize("{new_var_name}" := tally_function(value, na.rm=na.rm)) # aggregate according to tally_function
+    tidyr::pivot_longer(-.data$tmp_row_id) %>% # convert to long format
+    dplyr::group_by(.data$tmp_row_id) %>% # group by row
+    dplyr::summarize("{new_var_name}" := tally_function(.data$value, na.rm=na.rm)) # aggregate according to tally_function
   if(!is.null(join_function)){
-    df_tally <- df_id %>%
-      join_function(df_tally, by="..temp_id_col..")  # join aggregated scores with original dataframe using join_function
+    df_tally <- df %>%
+      join_function(df_tally, by="tmp_row_id")  # join aggregated scores with original dataframe using join_function
   }
-  df <- df %>% dplyr::select(-..temp_id_col..)
-  return(df)
+  df_tally <- df_tally %>% dplyr::select(-.data$tmp_row_id)
+  return(df_tally)
 }
