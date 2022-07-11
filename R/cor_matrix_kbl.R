89b56315ea75f40466608673ca48c1585979b9cc
Ashish <ashm@stanford.edu>
2022-07-11 06:46:50 -0700

    added option to return a correlation matrix dataframe

--- R/cor_matrix_kbl.R
+++ R/cor_matrix_kbl.R
@@ -21,14 +21,14 @@
 
 
 
-cor_matrix_kbl <- function(dat, column_grid=NULL, alpha=.05){
+cor_matrix_kbl <- function(dat, column_grid=NULL, alpha=.05, get_df=FALSE, round_digits=3){
   if(is.null(column_grid)){
     column_grid <- expand.grid(names(dat), names(dat))
   }
   cor_mat <- column_grid %>%
     dplyr::mutate(cor = purrr::map2_dbl(.[[1]], .[[2]], ~{
       stats::cor.test(dat[[.x]], dat[[.y]], use="complete.obs")$estimate
-      }
+    }
     )) %>%
     tidyr::pivot_wider(id_cols = 1,
                        names_from = 2,
@@ -36,37 +36,60 @@ cor_matrix_kbl <- function(dat, column_grid=NULL, alpha=.05){
   p_mat <- column_grid %>%
     dplyr::mutate(p = purrr::map2_dbl(.[[1]], .[[2]], ~{
       stats::cor.test(dat[[.x]], dat[[.y]], use="complete.obs")$p.value
-      }
+    }
     )) %>%
     tidyr::pivot_wider(id_cols = 1,
                        names_from = 2,
                        values_from = p)
   colnames(cor_mat)[1] <- " "
-  k <- cor_mat %>%
-    dplyr::mutate_if(is.numeric, ~round(., digits = 3)) %>%
-    kableExtra::kbl() %>%
-    kableExtra::kable_styling()
+  df <- cor_mat %>%
+    dplyr::mutate_if(is.numeric, ~round(., digits = round_digits))
 
-  for(colnum in 2:ncol(cor_mat)){
-    k <- k %>%
-      kableExtra::column_spec(colnum,
-                              background = dplyr::case_when(
-                                p_mat[,colnum] < alpha ~ dplyr::case_when(
-                                  cor_mat[,colnum] > 0  & cor_mat[,colnum] <= .2 ~ "#99e699",
-                                  cor_mat[,colnum] > .2  & cor_mat[,colnum] <= .4 ~ "#70db70",
-                                  cor_mat[,colnum] > .4  & cor_mat[,colnum] <= .6 ~ "#47d147",
-                                  cor_mat[,colnum] > .6  & cor_mat[,colnum] <= .8 ~ "#2eb82e",
-                                  cor_mat[,colnum] > .8  & cor_mat[,colnum] <= 1 ~ "#248f24",
-                                  cor_mat[,colnum] < 0  & cor_mat[,colnum] <= .2 ~ "#ffb3b3",
-                                  cor_mat[,colnum] < .2  & cor_mat[,colnum] <= .4 ~ "#ff8080",
-                                  cor_mat[,colnum] < .4  & cor_mat[,colnum] <= .6 ~ "#ff4d4d",
-                                  cor_mat[,colnum] < .6  & cor_mat[,colnum] <= .8 ~ "#ff1a1a",
-                                  cor_mat[,colnum] < .8  & cor_mat[,colnum] <= 1 ~ "#e60000",
-                                ),
-                                TRUE ~ "#ffffff")
-      )
+  if(!get_df){
+    # Return the kable table
+    k <- df %>%
+      kableExtra::kbl() %>%
+      kableExtra::kable_styling()
 
+    for(colnum in 2:ncol(cor_mat)){
+      result <- k %>%
+        kableExtra::column_spec(colnum,
+                                background = dplyr::case_when(
+                                  p_mat[,colnum] < alpha ~ dplyr::case_when(
+                                    cor_mat[,colnum] > 0  & cor_mat[,colnum] <= .2 ~ "#99e699",
+                                    cor_mat[,colnum] > .2  & cor_mat[,colnum] <= .4 ~ "#70db70",
+                                    cor_mat[,colnum] > .4  & cor_mat[,colnum] <= .6 ~ "#47d147",
+                                    cor_mat[,colnum] > .6  & cor_mat[,colnum] <= .8 ~ "#2eb82e",
+                                    cor_mat[,colnum] > .8  & cor_mat[,colnum] <= 1 ~ "#248f24",
+                                    cor_mat[,colnum] < 0  & cor_mat[,colnum] <= .2 ~ "#ffb3b3",
+                                    cor_mat[,colnum] < .2  & cor_mat[,colnum] <= .4 ~ "#ff8080",
+                                    cor_mat[,colnum] < .4  & cor_mat[,colnum] <= .6 ~ "#ff4d4d",
+                                    cor_mat[,colnum] < .6  & cor_mat[,colnum] <= .8 ~ "#ff1a1a",
+                                    cor_mat[,colnum] < .8  & cor_mat[,colnum] <= 1 ~ "#e60000",
+                                  ),
+                                  TRUE ~ "#ffffff")
+        )
+    }
 
+    return(result)
+  } else {
+    # Return the dataframe
+
+    result <- df %>%
+      dplyr::mutate_all(as.character)
+    for(col_num in 2:ncol(df)){
+
+      for(row_num in 2:nrow(df)){
+        if(p_mat[row_num, col_num] < .001){
+          result[row_num, col_num] <- paste(result[row_num, col_num], "***")
+        } else if(p_mat[row_num, col_num] < .01){
+          result[row_num, col_num] <- paste(result[row_num, col_num], "**")
+        } else if(p_mat[row_num, col_num] < .05){
+          result[row_num, col_num] <- paste(result[row_num, col_num], "*")
+        }
+      }
+    }
+    return(result)
   }
-  return(k)
+
 }
---
+++ tests/testthat/test-cor_matrix_kbl.R
@@ -0,0 +1,7 @@
+data("mtcars")
+cor_matrix_kbl(mtcars, get_df=TRUE)
+
+test_that("Placeholder", {
+
+  expect_true(TRUE)
+})
