ceaf2f7b593a064710f37cd2d872c3e6c3b21fe3
Ashish <ashm@stanford.edu>
2022-07-12 06:57:46 -0700

    fixed non informative warning

--- R/codebook_preproc.R
+++ R/codebook_preproc.R
@@ -83,7 +83,7 @@ codebook_recoder <- function(df, var_regex, values_from, values_to, FUN=as.numer
     if(!is.null(FUN)){
       safe_func <- function(x, ...) {
         tryCatch(expr=FUN(x, ...),
-                 warning=function(w) {warning("Unable to apply FUN to column"); return(x)},
+                 warning=function(w) {warning("Warning"); return(w)},
                  error=function(e) e)
       }
       df <- df %>%
